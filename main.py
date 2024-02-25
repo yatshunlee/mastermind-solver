0002e0b6e20c2c6ebfb214f90495850856c0a2fb
Jasper Lee <69416199+yatshunlee@users.noreply.github.com>
2024-02-25 18:31:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,39 @@ if __name__ == '__main__':
     guesses = create_all_comb()
 
     while True:
-        true = input('What is your secret code (type q to leave)? ')
+        # ask user's input
+        true = input(
+            'What is your secret code (in the format of a, b, c, d with a range of (0 - 6))? You can type q to leave as well: ')
+        # exit
         if true == 'q':
             print('Bye~~')
             break
-        possible_comb = all_comb    
         true = np.array(eval(f"[{true}]"))
+        
+        # check the true array
+        if np.any(true < 0) or np.any(true >= 6):
+            print('Invalid input.')
+            continue
+
+        possible_comb = all_comb
         while True:
+            # get the look-ahead contingency table
             score_table = make_score_table(guesses, possible_comb)
+            # calculate the entropies of each guess
             entropies = []
             for i in score_table.columns:
                 entropies.append(score_table[i].value_counts(
                     normalize=True).apply(
                     lambda p: - p * np.log2(p)).sum())
+            # make a guess with the largest entropy and check
             action = guesses[np.argmax(entropies)]
             res = scoring(action, true)
             print('AI chooses:', action, 'and gets a result:', res)
+            # update the possible combinations table
             possible_comb = possible_comb[
                 score_table[score_table[np.argmax(entropies)]==res
             ].index]
+            # break the calculation loop if it remains only 1 possible combination
             if possible_comb.shape[0] == 1:
                 print('AI guesses', possible_comb[0], 'as the final answer.')
                 break
